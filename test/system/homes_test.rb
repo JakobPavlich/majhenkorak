c53157403a50afad588ea3eb4ad57e770de4bacc
JakobPavlich <pavlich.jakob@gmail.com>
2026-08-19 09:51:30 +0200

    dodal še en majhen test

--- test/system/homes_test.rb
+++ test/system/homes_test.rb
@@ -11,4 +11,10 @@ class HomesTest < ApplicationSystemTestCase
 
     assert_selector "body"
   end
+
+  test "pravi naslov na prvi strani" do
+    visit root_url
+
+    assert_selector "h1", text: "MAJHEN KORAK"
+  end
 end
