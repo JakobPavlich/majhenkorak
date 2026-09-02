ca450afd2fb42a2d33fa73aee282ece1db520195
JakobPavlich <pavlich.jakob@gmail.com>
2026-09-02 14:32:38 +0200

    šel naprej z rails tečajem. views, controllers...novi dodatki. Ne dela še. Prav tako bi lahko še navigacijo malo oblikoval glede na CSS tečaj.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,15 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+  def show
+    params[:id]
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+  end
 end
