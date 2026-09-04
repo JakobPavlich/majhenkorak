ef6a708b04a1ef63f6444e4a4391bfeb50123c40
JakobPavlich <pavlich.jakob@gmail.com>
2026-09-04 14:48:12 +0200

    večinoma nastavljal vscode, sem pa šel tudi malo naprej po tečaju. Predvsem pri objavljanju objav, form...

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,5 +12,17 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post, notice: "Objava je bila objavljena"
+    else
+      render :new, status: :unprocessable_content
+    end
+  end
+
+  private
+
+  def post_params
+    params.expect(post: [ :title, :label, :favnumber, :region, :apply_url, :apply_email ])
   end
 end
