5ee47dacf30286db1903285542345bb469e64ca8
JakobPavlich <pavlich.jakob@gmail.com>
2026-09-18 14:36:26 +0200

    turnstile popravek

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,13 @@ import "./controllers";
 
 import "trix";
 import "@rails/actiontext";
+
+document.addEventListener("turbo:load", () => {
+  document.querySelectorAll(".cf-turnstile").forEach((element) => {
+    if (window.turnstile && !element.querySelector("iframe")) {
+      window.turnstile.render(element, {
+        sitekey: element.dataset.sitekey,
+      });
+    }
+  });
+});
