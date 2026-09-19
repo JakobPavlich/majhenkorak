873aee164785bd9852e251608c27d31475b7284a
JakobPavlich <pavlich.jakob@gmail.com>
2026-09-19 11:20:06 +0200

    dal v komentar dodatno koda v application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,12 +4,12 @@ import "./controllers";
 import "trix";
 import "@rails/actiontext";
 
-document.addEventListener("turbo:load", () => {
-  document.querySelectorAll(".cf-turnstile").forEach((element) => {
-    if (window.turnstile && !element.querySelector("iframe")) {
-      window.turnstile.render(element, {
-        sitekey: element.dataset.sitekey,
-      });
-    }
-  });
-});
+// document.addEventListener("turbo:load", () => {
+//   document.querySelectorAll(".cf-turnstile").forEach((element) => {
+//     if (window.turnstile && !element.querySelector("iframe")) {
+//       window.turnstile.render(element, {
+//         sitekey: element.dataset.sitekey,
+//       });
+//     }
+//   });
+// });
